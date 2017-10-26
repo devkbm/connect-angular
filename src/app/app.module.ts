3bddd88b11f2b7b732f2a002261a519def2993c6
devkbm <kbm0417@nate.com>
2017-10-26 19:31:28 +0900

    routing 공부중

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { CommonLayoutModule } from "./common-layout/common-layout.module"
+import { CommonLayoutModule } from "./common-layout/common-layout.module";
+import { TestModule } from './test/test.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,    
-    CommonLayoutModule
+    CommonLayoutModule,
+    TestModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/common-layout/common-layout-routing.module.ts
+++ src/app/common-layout/common-layout-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { TestComponent } from '../test/test/test.component';
+import { Test2Component } from '../test/test2/test2.component';
+
+const routes: Routes = [
+  {path: 'test', component: TestComponent},
+  {path: 'test2', component: Test2Component},
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

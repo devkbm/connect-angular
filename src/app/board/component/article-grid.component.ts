81142949e95dcb636a3bb64b0a665601b89c54e9
devkbm <kbm0417@nate.com>
2017-12-04 19:29:36 +0900

    게시판 모듈 수정중

---
+++ src/app/board/component/article-grid.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-article-grid',
+  templateUrl: './article-grid.component.html',
+  styles: ['']
+})
+export class ArticleGridComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/board/model/article.ts
@@ -0,0 +1,12 @@
+export class Article {
+    pkArticle: number;
+    ppkArticle: number;
+    title: string;
+    contents: string;
+    pwd: string;
+    hitCnt: string;
+    fromdDt: string;
+    toDt: string;    
+    seq: number;
+    depth: number;    
+}

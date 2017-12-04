9af2e5bc7d02a980da8122fb58f1aa972bc1d7e7
kbm <kbm0417@nate.com>
2017-12-05 00:08:55 +0900

    게시판 모듈 수정중

--- src/app/board/service/board.service.ts
+++ src/app/board/service/board.service.ts
@@ -11,12 +11,13 @@ import 'rxjs/add/observable/throw';
 import { ResponseObject } from '../../common-layout/model/response-object';
 import { ResponseList } from '../../common-layout/model/response-list';
 import { Board } from '../model/board';
+import { Article } from '../model/article';
 
 @Injectable()
 export class BoardService {
 
     private API_URI = 'http://localhost:8090/grw';
-    
+
     constructor(private http: HttpClient) { }
 
     getBoardList(): Observable<ResponseList<Board>> {
@@ -36,7 +37,7 @@ export class BoardService {
             })
             .catch((err) => Observable.throw(err));
     }
-    
+
     saveBoard(board: Board): Observable<ResponseObject<Board>> {
         const url = `${this.API_URI}/boards/${board.pkBoard}`;
 
@@ -58,5 +59,23 @@ export class BoardService {
             })
             .catch((err) => Observable.throw(err));
     }
-    
+
+    getArticleList(fkBoard: string, title?: string, contents?: string): Observable<ResponseList<Board>> {
+        let url = `${this.API_URI}/boards/articles?fkBoard=${fkBoard}`;
+
+        if ( title !== '' ) {
+            url = url + '&title=' + title;
+        }
+
+        if ( contents !== '' ) {
+            url = url + '&contents=' + contents;
+        }
+
+        return this.http.get(url)
+            .map((response: Response) => {
+                return response;
+            })
+            .catch((err) => Observable.throw(err));
+    }
+
 }
